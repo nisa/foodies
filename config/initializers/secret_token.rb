6ee5b201aa8def35b74fc449759e1070fd34f67b
nisabalakrishnan <nisa@mobme.in>
2013-01-10 14:07:45 +0530

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodies::Application.config.secret_token = 'f9f97f205c6957b1b0314684f93df750d3089b429b5ad2ea47ccd4212e1027d92cfb17d8c9144385d4296ce60f226bdcd7e48e3e0a9e6ef0cd0bced104d18a12'
